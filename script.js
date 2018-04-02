a1787de7062831b0c462c47de5ebbb1a81853b02
Kacper Stroisz <kacperstroisz@wp.pl>
2018-04-02 08:18:47 +0200

    Change .js

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ console.log(multiply(5));
 
 const average = (...args) => args.reduce((a, b) => a + b) / args.length;
 
+console.log(average(1));
+console.log(average(1, 3));
+console.log(average(1, 3, 6, 6));
+
 /* 4 */
 
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
